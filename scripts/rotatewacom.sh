d3b5a6cccbd52aec24ccd86d0dd62959946e85cc
Nathan Typanski <ntypanski@gmail.com>
2013-05-19 14:44:41 -0400

    Lots of config updates. Switched to i3.

---
+++ scripts/rotatewacom.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+device="Wacom Intuos4 6x9"
+stylus="$device Pen stylus"
+eraser="$device Pen eraser"
+touch="$device Finger touch"
+pad="$device Finger pad"
+
+xsetwacom --set "$stylus" Rotate $1
+xsetwacom --set "$eraser" Rotate $1
+xsetwacom --set "$touch"  Rotate $1
+xsetwacom --set "$pad"    Rotate $1
