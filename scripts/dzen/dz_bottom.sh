d7e5f0e811a38b9746d7108d3f2fe00d2ef83093
Nathan Typanski <ntypanski@gmail.com>
2013-05-27 14:08:24 -0400

    Changes dzen startup organization

--- scripts/dzen/dz_bottom.sh
+++ scripts/dzen/dz_bottom.sh
@@ -3,6 +3,6 @@
 FG='#839496'
 BG='#00141a'
 FONT='Terminus-8'
-CONKYCONFIG='.conkybottomrc'
+CONKYCONFIG='scripts/dzen/.conkybottomrc'
 
 conky -c $CONKYCONFIG | dzen2 -ta r -h '16' -x '300' -y '884' -w '800' -bg $BG -fg $FG -fn $FONT -e 'button2=' &
--- scripts/dzen/dz_top.sh
+++ scripts/dzen/dz_top.sh
@@ -3,6 +3,6 @@
 FG='#839496'
 BG='#00141a'
 FONT='Terminus-8'
-CONKYCONFIG='.conkytoprc'
+CONKYCONFIG='scripts/dzen/.conkytoprc'
 
-conky -c '/home/nathan/.conkytoprc' | dzen2 -dock -ta r -h '16' -w '1600' -bg $BG -fg $FG -fn $FONT -e 'button2=' &
+conky -c $CONKYCONFIG | dzen2 -dock -ta r -h '16' -w '1600' -bg $BG -fg $FG -fn $FONT -e 'button2=' &
