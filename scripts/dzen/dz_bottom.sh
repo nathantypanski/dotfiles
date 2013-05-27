af08751262af801df7a99e20bee64c10f99f089f
Nathan Typanski <ntypanski@gmail.com>
2013-05-27 14:01:31 -0400

    Fixes dzen scripts

--- scripts/dzen/dz_bottom.sh
+++ scripts/dzen/dz_bottom.sh
@@ -3,6 +3,6 @@
 FG='#839496'
 BG='#00141a'
 FONT='Terminus-8'
-CONKYCONFIG='/home/nathan/.conkybottomrc'
+CONKYCONFIG='.conkybottomrc'
 
 conky -c $CONKYCONFIG | dzen2 -ta r -h '16' -x '300' -y '884' -w '800' -bg $BG -fg $FG -fn $FONT -e 'button2=' &
--- scripts/dzen/dz_top.sh
+++ scripts/dzen/dz_top.sh
@@ -3,6 +3,6 @@
 FG='#839496'
 BG='#00141a'
 FONT='Terminus-8'
-CONKYCONFIG='/home/nathan/.conkytoprc'
+CONKYCONFIG='.conkytoprc'
 
-(conky -c '/home/nathan/.conkytoprc'; cat '~/.zshrc')| dzen2 -dock -ta r -h '16' -w '1600' -bg $BG -fg $FG -fn $FONT -e 'button2=' &
+conky -c '/home/nathan/.conkytoprc' | dzen2 -dock -ta r -h '16' -w '1600' -bg $BG -fg $FG -fn $FONT -e 'button2=' &
