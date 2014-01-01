aaf4d10acdc0953c62fac10585bce0a0074d1cb3
Nathan Typanski <ntypanski@gmail.com>
2014-01-01 16:22:40 -0500

    uses gpg-agent for gpg keys

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -16,3 +16,11 @@ eval $(keychain --eval --agents ssh -Q --quiet $(cat ~/.keychain/keyfiles))
 for sd_cmd in systemctl systemd-analyze systemd-run; do
     alias $sd_cmd='DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/dbus/user_bus_socket" '$sd_cmd
 done
+
+if [ $EUID -ne 0 ] ; then
+    envfile="$HOME/.gpg-agent-info"
+    if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
+        eval "$(cat "$envfile")"
+        export GPG_AGENT_INFO  # the env file does not contain the export statement
+    fi
+fi
