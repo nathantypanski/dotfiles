b9e3f8af171a4a0b88f79ab8d98a31fb8e1774e0
Nathan Typanski <ntypanski@gmail.com>
2014-01-03 14:22:08 -0500

    use dzen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,7 +12,7 @@ import XMonad.Actions.DynamicWorkspaces ( addWorkspacePrompt
                                         , withWorkspace
                                         , withNthWorkspace
                                         , selectWorkspace)
--- import XMonad.Hooks.EwmhDesktops hiding (fullscreenEventHook)
+import XMonad.Hooks.EwmhDesktops (ewmhDesktopsLogHook)
 import XMonad.Actions.Navigation2D ( Navigation2D
                                    , lineNavigation
                                    , centerNavigation
@@ -27,6 +27,7 @@ import XMonad.Actions.Navigation2D ( Navigation2D
                                    , Direction2D
                                    )
 import XMonad.Hooks.DynamicLog
+import XMonad.Actions.TagWindows (addTag, tagDelPrompt, tagPrompt)
 
 -- functions for tagging windows and selecting them by tags
 -- import XMonad.Actions.TagWindows
@@ -34,11 +35,17 @@ import XMonad.Hooks.DynamicLog
 -- Better ways to spawn apps; safeSpawn and unsafeSpawn
 import XMonad.Util.Run
 
+-- dzen
+import qualified System.Dzen as D
+import XMonad.Util.Loggers ( logCmd, loadAvg, date, battery )
+import System.Process (CmdSpec (RawCommand))
+
 -- Hints on windows
 import XMonad.Layout.LayoutHints
 
 import XMonad.Prompt
 import XMonad.Prompt.Shell
+import XMonad.Prompt.Window
 
 -- Nice workspace bindings
 import XMonad.Actions.CycleWS ( toggleWS )
@@ -51,13 +58,23 @@ import qualified Data.Map        as M
 
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
+
 myBorderWidth   = 1
+
+myWorkspaces :: [String]
 myWorkspaces    = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14",
                    "15","16","17","18","19","20","21","22"]
+
+myModMask :: KeyMask
 myModMask       = mod4Mask
+
+myNormalBorderColor :: String
 myNormalBorderColor  = "#282a2e"
+
+myFocusedBorderColor :: String
 myFocusedBorderColor = "#a54242"
 
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask,   xK_Return), windows W.swapMaster)
     , ((modm .|. shiftMask,   xK_r     ), renameWorkspace defaultXPConfig)
@@ -82,11 +99,14 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,                 xK_period), sendMessage (IncMasterN (-1)))
     , ((modm,                 xK_b     ), sendMessage ToggleStruts)
     , ((modm .|. shiftMask,   xK_q     ), io exitSuccess)
-    , ((modm,                 xK_q     ), spawn "ghc ~/.xmonad/xmonad.hs; userctl restart xmonad")
+    , ((modm,                 xK_q     ), spawn "ghc ~/.xmonad/xmonad.hs; systemctl --user restart xmonad")
     , ((controlMask,          xK_Print ), spawn "sleep 0.2; scrot -s")
     , ((0,                    xK_Print ), spawn "scrot")
     , ((modm,                 xK_o     ), toggleWS)
     , ((modm,                 xK_p     ), shellPrompt myXPConfig)
+    , ((modm,                 xK_slash ), windowPromptGoto defaultXPConfig { autoComplete = Just 500000 } )
+    , ((modm,                 xK_m  ), tagPrompt defaultXPConfig (\s -> withFocused (addTag s)))
+    , ((modm,                 xK_apostrophe  ), tagDelPrompt defaultXPConfig)
 
       -- Prompt for a workspace to switch to
     , ((modm,                 xK_v     ), selectWorkspace myXPConfig)
@@ -136,18 +156,17 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     where workspaceKeys = [xK_1 .. xK_9] ++ [xK_0] ++ [xK_F1 .. xK_F12]
 
 
-myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
-
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList 
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster))
+    [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
+                                       >> windows W.shiftMaster)
 
     -- mod-button2, Raise the window to the top of the stack
-    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+    , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
 
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
+    , ((modm, button3), \w -> focus w >> mouseResizeWindow w
+                                      >> windows W.shiftMaster)
     ]
 
 myLayout = avoidStruts $
@@ -167,6 +186,7 @@ myLayout = avoidStruts $
             -- Percent of screen to increment by when resizing panes
             delta   = 3/100
 
+myManageHook :: ManageHook
 myManageHook = manageDocks
     <+> composeAll
         [ className =? "MPlayer"        --> doFloat
@@ -179,13 +199,18 @@ myManageHook = manageDocks
     <+>
         composeOne [isFullscreen -?> doFullFloat]
 
+myEventHook :: Event -> X All
 myEventHook = mempty
-myLogHook = mempty
+
+myLogHook :: X ()
+myLogHook = ewmhDesktopsLogHook
+
+myStartupHook :: X ()
 myStartupHook = return ()
 
 myXPConfig :: XPConfig
 myXPConfig =
-    XPC { font              = "-*-terminus-medium-r-*-*-*-120-75-75-*-*-iso8859-15"
+    XPC { XMonad.Prompt.font = "-*-terminus-medium-r-*-*-*-120-75-75-*-*-iso8859-15"
         , bgColor           = "grey22"
         , fgColor           = "grey80"
         , fgHLight          = "black"
@@ -206,23 +231,48 @@ myXPConfig =
         , alwaysHighlight   = True
         }
 
-myConfig = defaultConfig {
-        focusFollowsMouse  = myFocusFollowsMouse,
-        borderWidth        = myBorderWidth,
-        modMask            = myModMask,
-        workspaces         = myWorkspaces,
-        normalBorderColor  = myNormalBorderColor,
-        focusedBorderColor = myFocusedBorderColor,
-        keys               = myKeys,
-        mouseBindings      = myMouseBindings,
-        layoutHook         = myLayout,
-        manageHook         = myManageHook,
-        handleEventHook    = myEventHook,
-        logHook            = myLogHook,
-        startupHook        = myStartupHook
-    }
+myDzenPP :: PP
+myDzenPP = defaultPP { ppCurrent = dzenColor "#005f00" "#afd700" . pad
+                     , ppVisible = dzenColor "#002b36" "#839496" . pad
+                     , ppHidden = dzenColor "#eeeeee" "#808080" . pad
+                     , ppUrgent = dzenColor "#eeeeee" "#d70000" . pad
+                     , ppExtras = [
+                                      logCmd "echo -n '^fg(#81a2be)^i(.dzen/icons/arch_10x10.xbm)^fg()'"
+                                    , loadAvg
+                                    , logCmd "echo -n '^fg(#81a2be)^i(.dzen/icons/clock.xbm)^fg()'"
+                                    , date "%r"
+                                    , logCmd "echo -n '^fg(#81a2be)^i(.dzen/icons/bat_full_01.xbm)^fg()'"
+                                    , battery
+                       -- , logCmd "echo -n '^fg(#81a2be)^i(.dzen/icons/mail.xbm)^fg()' $(~/bin/gmail.py)"
+                                    ]
+                     , ppHiddenNoWindows = const ""
+                     , ppWsSep = ""
+                     , ppSep = " "
+                     , ppLayout = dzenColor "#1c1c1c" "#d0d0d0" .
+                                    pad . (\ x -> case x of
+                                              "Tall" -> "||"
+                                              "Mirror Tall" -> "="
+                                              "Hinted Full" -> "[H]"
+                                              "Hinted Tall" -> "|H|"
+                                              _ -> x
+                                    )
+                     , ppTitle = wrap "^ca(2,xdotool key super+shift+c)" "^ca()" . dzenColor "#c5c8c6" "#282a2e" . shorten 40
+                     }
 
-main = xmonad defaultConfig {
+dzenCommand :: CmdSpec
+dzenCommand = RawCommand "dzen2"
+    ["-ta","l"
+    ,"-fg","#eeeeee"
+    ,"-bg","#1D1F21"
+    ,"-w", "1600"
+    ,"-e","button2=;"
+    ,"-fn", "-*-terminus-medium-r-*-*-12-120-*-*-*-*-iso8859-*"
+    ]
+
+main :: IO ()
+main = do
+    dzw <- D.createDzen dzenCommand
+    xmonad defaultConfig {
             focusFollowsMouse  = myFocusFollowsMouse,
             borderWidth        = myBorderWidth,
             modMask            = myModMask,
@@ -234,6 +284,10 @@ main = xmonad defaultConfig {
             layoutHook         = myLayout,
             manageHook         = myManageHook,
             handleEventHook    = myEventHook,
-            logHook            = myLogHook,
+            logHook            = ewmhDesktopsLogHook <+>
+                dynamicLogWithPP myDzenPP
+                    {
+                        ppOutput = hPutStrLn dzw
+                    },
             startupHook        = myStartupHook
         }
