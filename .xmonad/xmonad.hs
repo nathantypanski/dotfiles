bde75162a7604f7186366704c42f628d5cded46a
Nathan Typanski <ntypanski@gmail.com>
2014-02-02 02:46:17 -0500

    xmonad sweetness

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,119 +1,86 @@
 import XMonad
 import XMonad.Hooks.ManageDocks (Direction2D(L, R, U, D)
                                 ,ToggleStruts(..)
-                                ,manageDocks
-                                ,avoidStruts)
+                                ,manageDocks, avoidStruts)
 import Data.List (isPrefixOf)
 import XMonad.Actions.CopyWindow (copy)
-import Data.Monoid (All
-                   ,mempty)
+import Data.Monoid (All, mempty)
 import System.Exit (exitSuccess)
 import XMonad.Layout.NoBorders (smartBorders)
-import XMonad.Layout.Groups.Examples (tallTabs
-                                     ,defaultTiledTabsConfig)
-import XMonad.Hooks.ManageHelpers (isFullscreen
-                                  ,doFullFloat
-                                  ,composeOne
-                                  ,(-?>))
-import XMonad.Actions.DynamicWorkspaces (addWorkspacePrompt
-                                        ,removeWorkspace
-                                        ,renameWorkspace
-                                        ,withWorkspace
-                                        ,withNthWorkspace
-                                        ,selectWorkspace)
+import XMonad.Layout.Groups.Examples (tallTabs, defaultTiledTabsConfig)
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat
+                                  ,composeOne, (-?>))
+import XMonad.Actions.DynamicWorkspaces (addWorkspacePrompt, removeWorkspace
+                                        ,renameWorkspace, withWorkspace
+                                        ,withNthWorkspace, selectWorkspace)
 import XMonad.Hooks.EwmhDesktops (ewmhDesktopsLogHook)
-import XMonad.Actions.Navigation2D (Navigation2D
-                                   ,lineNavigation
-                                   ,centerNavigation
-                                   ,fullScreenRect
-                                   ,singleWindowRect
-                                   ,switchLayer
-                                   ,windowGo
-                                   ,windowSwap
-                                   ,windowToScreen
-                                   ,screenGo
-                                   ,screenSwap
-                                   ,Direction2D)
+import XMonad.Actions.Navigation2D (Navigation2D, Direction2D
+                                   ,lineNavigation, centerNavigation
+                                   ,fullScreenRect, singleWindowRect
+                                   ,switchLayer, windowGo, windowSwap
+                                   ,windowToScreen, screenGo, screenSwap)
 import XMonad.Actions.GridSelect (defaultGSConfig,
                                   goToSelected)
-import XMonad.Hooks.DynamicLog (PP
-                               ,dynamicLogString
-                               ,dynamicLogWithPP
-                               ,pad
-                               ,dzenColor
-                               ,ppTitle
-                               ,ppLayout
-                               ,defaultPP
-                               ,ppCurrent
-                               ,ppVisible
-                               ,ppHidden
-                               ,ppHiddenNoWindows
-                               ,ppUrgent
-                               ,ppSep
-                               ,ppOutput
-                               ,ppWsSep
-                               ,ppExtras
-                               ,wrap
-                               ,shorten)
-import XMonad.Actions.TagWindows (addTag
-                                 ,tagDelPrompt
-                                 ,tagPrompt)
+import XMonad.Hooks.DynamicLog (PP, dynamicLogString, dynamicLogWithPP
+                               ,pad, dzenColor, ppTitle, ppLayout, defaultPP
+                               ,ppCurrent, ppVisible, ppHidden
+                               ,ppHiddenNoWindows, ppUrgent, ppSep
+                               ,ppOutput, ppWsSep, ppExtras, wrap, shorten)
+import XMonad.Actions.TagWindows (addTag, tagDelPrompt, tagPrompt)
 import XMonad.Util.Run (hPutStrLn)
-import qualified XMonad.Util.Dzen as XD
+import XMonad.Util.Dzen (dzenConfig, timeout, font, (>=>))
 import qualified System.Dzen as D
-import XMonad.Util.Loggers (logCmd
-                           ,loadAvg
-                           ,date
-                           ,battery)
+import XMonad.Util.Loggers (Logger, logCmd, loadAvg, date, battery)
 import System.Process (CmdSpec (RawCommand))
 import XMonad.Layout.LayoutHints (layoutHints)
-import XMonad.Prompt (XPConfig (..)
-                     ,XPPosition(Top)
-                     ,font
-                     ,bgColor
-                     ,defaultXPKeymap
-                     ,fgColor
-                     ,fgHLight
-                     ,bgHLight
-                     ,borderColor
-                     ,promptBorderWidth
-                     ,promptKeymap
-                     ,completionKey
-                     ,changeModeKey
-                     ,position
-                     ,height
-                     ,historySize
-                     ,historyFilter
-                     ,defaultText
-                     ,autoComplete
-                     ,showCompletionOnTab
-                     ,searchPredicate
-                     ,alwaysHighlight)
+import XMonad.Prompt (XPConfig (..), XPPosition(Top)
+                     ,font, bgColor, defaultXPKeymap, fgColor, fgHLight
+                     ,bgHLight, borderColor, promptBorderWidth, promptKeymap
+                     ,completionKey, changeModeKey, position, height
+                     ,historySize, historyFilter, defaultText, autoComplete
+                     ,showCompletionOnTab, searchPredicate, alwaysHighlight)
 import XMonad.Prompt.Shell (shellPrompt)
 import XMonad.Prompt.Window (windowPromptGoto)
-
--- Nice workspace bindings
 import XMonad.Actions.CycleWS ( toggleWS )
+import XMonad.StackSet (swapMaster, shiftMaster, focusDown, focusMaster, sink
+                       ,greedyView, shift, view)
 
--- control focus
-import qualified XMonad.StackSet as W
+import Data.Map (Map, fromList)
 
--- key/mouse bindings
-import qualified Data.Map        as M
-
--- Colors
-
+colorBackground :: String
 colorBackground = "#1D1F21"
+
+colorCurrentLine :: String
 colorCurrentLine = "#282A2E"
+
+colorSelection :: String
 colorSelection = "#373B41"
+
+colorForeground :: String
 colorForeground = "#C5C8C6"
+
+colorComment :: String
 colorComment = "#707880"
+
+colorRed :: String
 colorRed = "#CC6666"
+
+colorOrange :: String
 colorOrange = "#DE935F"
+
+colorYellow :: String
 colorYellow = "#F0C674"
+
+colorGreen :: String
 colorGreen = "#B5BD68"
+
+colorAqua :: String
 colorAqua = "#8ABEB7"
+
+colorBlue :: String
 colorBlue = "#81A2BE"
+
+colorPurple :: String
 colorPurple = "#B294BB"
 
 myFocusFollowsMouse :: Bool
@@ -122,11 +89,10 @@ myFocusFollowsMouse = False
 myBorderWidth   = 1
 
 myWorkspaces :: [String]
-myWorkspaces    = ["1","2","3:web","4","5","6","7","8","9","10","11","12","13","14",
-                   "15","16","17","18","19","20","21","22"]
+myWorkspaces = map show [1..22]
 
 myModMask :: KeyMask
-myModMask       = mod4Mask
+myModMask = mod4Mask
 
 myNormalBorderColor :: String
 myNormalBorderColor  = colorComment
@@ -137,21 +103,26 @@ myFocusedBorderColor = colorForeground
 terminus :: String
 terminus = "-*-terminus-medium-r-*-*-12-120-*-*-*-*-iso8859-*"
 
+restartCommand :: String
+restartCommand =  "if type xmonad;" 
+                        ++ "then xmonad --recompile && xmonad --restart;"
+                        ++ "else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
+
 shiftLayout :: X ()
 shiftLayout = 
     sendMessage NextLayout
     >> (dynamicLogString myLayoutDzen 
-        >>= XD.dzenConfig (XD.timeout 0.5))
+        >>= dzenConfig (timeout 0.5 >=> XMonad.Util.Dzen.font terminus))
 
-myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ ((modm .|. shiftMask,   xK_Return), windows W.swapMaster)
+myKeys :: XConfig Layout -> Map (KeyMask, KeySym) (X ())
+myKeys conf@(XConfig {XMonad.modMask = modm}) = fromList $
+    [ ((modm .|. shiftMask,   xK_Return), windows swapMaster)
     , ((modm .|. shiftMask,   xK_r     ), renameWorkspace myXPConfig)
     , ((modm .|. shiftMask,   xK_c     ), kill)
     , ((modm, xK_space                 ), shiftLayout)
     , ((modm .|. shiftMask,   xK_space ), setLayout $ XMonad.layoutHook conf)
     , ((modm,                 xK_n     ), refresh)
-    , ((modm,                 xK_Tab   ), windows W.focusDown)
+    , ((modm,                 xK_Tab   ), windows focusDown)
     , ((modm,                 xK_h     ), windowGo   L False)
     , ((modm,                 xK_l     ), windowGo   R False)
     , ((modm,                 xK_k     ), windowGo   U False)
@@ -160,29 +131,33 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask,   xK_l     ), windowSwap R False)
     , ((modm .|. shiftMask,   xK_k     ), windowSwap U False)
     , ((modm .|. shiftMask,   xK_j     ), windowSwap D False)
-    , ((modm,                 xK_m     ), windows W.focusMaster)
+    , ((modm,                 xK_m     ), windows focusMaster)
     , ((modm .|. controlMask, xK_h     ), sendMessage Shrink)
     , ((modm .|. controlMask, xK_l     ), sendMessage Expand)
     , ((modm,                 xK_s     ), goToSelected defaultGSConfig)
-    , ((modm,                 xK_t     ), withFocused $ windows . W.sink)
+    , ((modm,                 xK_t     ), withFocused $ windows . sink)
     , ((modm,                 xK_comma ), sendMessage (IncMasterN 1))
     , ((modm,                 xK_period), sendMessage (IncMasterN (-1)))
     , ((modm,                 xK_b     ), sendMessage ToggleStruts)
     , ((modm .|. shiftMask,   xK_q     ), io exitSuccess)
-    , ((modm,                 xK_q     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
+    , ((modm,                 xK_q     ), spawn restartCommand)
     , ((controlMask,          xK_Print ), spawn "sleep 0.2; scrot -s")
     , ((0,                    xK_Print ), spawn "scrot")
     , ((modm,                 xK_o     ), toggleWS)
     , ((modm,                 xK_p     ), shellPrompt myXPConfig)
-    , ((modm,                 xK_slash ), windowPromptGoto myXPConfig { autoComplete = Just 500000 } )
-    , ((modm,                 xK_m     ), tagPrompt myXPConfig $ withFocused . addTag)
+    , ((modm,                 xK_slash ), windowPromptGoto myXPConfig {
+                                              autoComplete = Just 500000 
+                                          })
+    , ((modm,                 xK_m     ), tagPrompt 
+                                            myXPConfig $ withFocused . addTag)
     , ((modm .|. shiftMask,   xK_m     ), tagDelPrompt myXPConfig)
 
       -- Prompt for a workspace to switch to
     , ((modm,                 xK_v     ), selectWorkspace myXPConfig)
 
       -- Prompt for a workspace and copy all client from the current one there
-    , ((modm .|. controlMask, xK_m     ), withWorkspace myXPConfig (windows . copy))
+    , ((modm .|. controlMask, xK_m     ), withWorkspace 
+                                            myXPConfig (windows . copy))
 
       -- Remove current workspace (must be empty)
     , ((modm .|. shiftMask             , xK_BackSpace  ), removeWorkspace)
@@ -207,12 +182,12 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) workspaceKeys
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
     ++
 
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) workspaceKeys
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
     ++
 
     --
@@ -221,22 +196,22 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(view, 0), (shift, shiftMask)]]
 
     where workspaceKeys = [xK_1 .. xK_9] ++ [xK_0] ++ [xK_F1 .. xK_F12]
 
-
-myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
+myMouseBindings :: XConfig t -> Map (KeyMask, Button) (Window -> X ()) 
+myMouseBindings (XConfig {XMonad.modMask = modm}) = fromList
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster)
+                                       >> windows shiftMaster)
 
     -- mod-button2, Raise the window to the top of the stack
-    , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
+    , ((modm, button2), \w -> focus w >> windows shiftMaster)
 
     -- mod-button3, Set the window to floating mode and resize by dragging
     , ((modm, button3), \w -> focus w >> mouseResizeWindow w
-                                      >> windows W.shiftMaster)
+                                      >> windows shiftMaster)
     ]
 
 myLayout = avoidStruts $
@@ -268,7 +243,6 @@ myManageHook = manageDocks
         -- Float Firefox dialog windows
         , (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat
         , resource  =? "desktop_window" --> doIgnore
-        , resource  =? "kdesktop"       --> doIgnore
         ]
     <+>
         composeOne [isFullscreen -?> doFullFloat]
@@ -295,7 +269,7 @@ myXPConfig =
         , completionKey     = xK_Tab
         , changeModeKey     = xK_grave
         , position          = Top
-        , height            = 18
+        , height            = 14
         , historySize       = 256
         , historyFilter     = id
         , defaultText       = []
@@ -305,6 +279,7 @@ myXPConfig =
         , alwaysHighlight   = True
         }
 
+myEmptyDzen :: PP
 myEmptyDzen = defaultPP
     { ppCurrent = const ""
     , ppVisible = const ""
@@ -317,6 +292,7 @@ myEmptyDzen = defaultPP
     , ppLayout = const ""
     }
 
+myLayoutDzen :: PP
 myLayoutDzen = myEmptyDzen
     { ppLayout = dzenColor colorComment colorBackground .
                 pad . (\ x -> case x of
@@ -330,18 +306,11 @@ myLayoutDzen = myEmptyDzen
     }
 
 myDzenPP :: PP
-myDzenPP = defaultPP { ppCurrent = dzenColor colorBackground colorGreen . pad
-                     , ppVisible = dzenColor colorForeground colorSelection . pad
-                     , ppHidden = dzenColor colorComment colorCurrentLine . pad
-                     , ppUrgent = dzenColor colorBackground colorRed . pad
-                     , ppExtras = [
-                                      logCmd $ "echo -n '^fg(" ++ colorBlue ++")^fg()'"
-                                    , loadAvg
-                                    , logCmd $ "echo -n '^fg(" ++ colorYellow ++ ")^i(.dzen/icons/clock.xbm)^fg()'"
-                                    , date "%r"
-                                    , logCmd $ "echo -n '^fg(" ++ colorAqua ++ ")^i(.dzen/icons/bat_full_01.xbm)^fg()'"
-                                    , battery
-                                    ]
+myDzenPP = defaultPP { ppCurrent = dzenColorPad colorBackground colorGreen
+                     , ppVisible = dzenColorPad colorForeground colorSelection
+                     , ppHidden = dzenColorPad colorComment colorCurrentLine
+                     , ppUrgent = dzenColorPad colorBackground colorRed
+                     , ppExtras = status
                      , ppHiddenNoWindows = const ""
                      , ppWsSep = ""
                      , ppSep = " "
@@ -354,8 +323,25 @@ myDzenPP = defaultPP { ppCurrent = dzenColor colorBackground colorGreen . pad
                                               "Tabs by Vertical" -> "#"
                                               _ -> x
                                     )
-                     , ppTitle = wrap "^ca(2,xdotool key super+shift+c)" "^ca()" . dzenColor "#c5c8c6" "#282a2e" . shorten 40
+                     , ppTitle = wrap "^ca(2,xdotool key super+shift+c)" 
+                                      "^ca()" 
+                                    . dzenColor "#c5c8c6" "#282a2e" . shorten 40
                      }
+    where
+        dzenColorPad :: String -> String -> String -> String
+        dzenColorPad bg fg = dzenColor bg fg . pad
+        status :: [XMonad.Util.Loggers.Logger]
+        status = [loadAvg
+                ,logCmd $ "echo -n '^fg(" 
+                        ++ colorYellow 
+                        ++ ")^i(.dzen/icons/clock.xbm)^fg()'"
+                ,date "%r"
+                ,logCmd $ "echo -n '^fg(" 
+                        ++ colorAqua 
+                        ++ ")^i(.dzen/icons/bat_full_01.xbm)^fg()'"
+                ,battery
+                ]
+
 
 dzenPath :: FilePath
 dzenPath = "/usr/bin/dzen2"
