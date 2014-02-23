01f5cadb5a03be806d6de696ce3a322f2472f0cd
Nathan Typanski <ntypanski@gmail.com>
2014-02-23 01:43:28 -0500

    fix ghc-pkg functions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -83,22 +83,25 @@ function open-urxvt-here () { urxvt &! }
 # unregister broken GHC packages. Run this a few times to resolve dependency rot in installed packages.
 # ghc-pkg-clean -f cabal/dev/packages*.conf also works.
 function ghc-pkg-clean() {
-for p in `ghc-pkg check $* 2>&1 | grep problems | awk '{print $6}' | sed -e 's/:$//'`
-do
-echo unregistering $p; ghc-pkg $* unregister $p
-done
+  for p in `ghc-pkg check $* 2>&1  | grep problems | awk '{print $6}' | sed -e 's/:$//'`
+  do
+    echo unregistering $p; ghc-pkg $* unregister $p
+  done
 }
- 
+
 # remove all installed GHC/cabal packages, leaving ~/.cabal binaries and docs in place.
 # When all else fails, use this to get out of dependency hell and start over.
 function ghc-pkg-reset() {
-read -p 'erasing all your user ghc and cabal packages - are you sure (y/n) ? ' ans
-test x$ans == xy && ( \
-echo 'erasing directories under ~/.ghc'; rm -rf `find ~/.ghc -maxdepth 1 -type d`; \
-echo 'erasing ~/.cabal/lib'; rm -rf ~/.cabal/lib; \
-# echo 'erasing ~/.cabal/packages'; rm -rf ~/.cabal/packages; \
-# echo 'erasing ~/.cabal/share'; rm -rf ~/.cabal/share; \
-)
+  if [[ $(readlink -f /proc/$$/exe) =~ zsh ]]; then
+    read 'ans?Erasing all your user ghc and cabal packages - are you sure (y/N)? '
+  else # assume bash/bash compatible otherwise
+    read -p 'Erasing all your user ghc and cabal packages - are you sure (y/N)? ' ans
+  fi
+
+  [[ x$ans =~ "xy" ]] && ( \
+    echo 'erasing directories under ~/.ghc'; command rm -rf `find ~/.ghc/* -maxdepth 1 -type d`; \
+    echo 'erasing ~/.cabal/lib'; command rm -rf ~/.cabal/lib; \
+  )
 }
- 
-alias cabalupgrades="cabal list --installed | egrep -iv '(synopsis|homepage|license)'"
+
+alias cabalupgrades="cabal list --installed  | egrep -iv '(synopsis|homepage|license)'"
