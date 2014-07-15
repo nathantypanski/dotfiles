a0871a589955a01289f17e8238f1b71dce9aac5a
Nathan Typanski <ntypanski@gmail.com>
2014-07-15 19:33:05 -0400

    tweak shell

--- .zsh/autostarts.zsh
+++ .zsh/autostarts.zsh
@@ -1,4 +1,3 @@
-
 # Grep for a process name, excluding grep from the output.
 function psgrep() {
     ps aux | grep $1 | grep -v grep
@@ -7,17 +6,16 @@ function psgrep() {
 # Spawn a daemonized script ($1) only if it is not already started.
 # If $2 is provided, this will run the daemon in that folder.
 function daemonize_script () {
-    if [[ -n $2 ]]; then
-        pushd
-        cd $2
-    fi
-    exists=$(psgrep $1)
-    if [[ -z "$exists" ]]; then
-        nohup $1
-    fi
-    if [[ -n $2 ]]; then
-        popd
+    if [[ -z $(psgrep $1) ]]; then
+        if [[ -n $2 ]]; then
+            olddir=$(pwd)
+            cd $2
+        fi
+        nohup $1 > /dev/null 2>&1 &
+        if [[ -n $2 ]]; then
+            cd $olddir
+        fi
     fi
 }
 
-daemonize_script gitit ~/wiki > /dev/null 2>&1 &
+daemonize_script ~/bin/start_gitit ~/.gitit
