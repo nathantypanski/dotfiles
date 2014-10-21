4f483ad15f7959389d1f3f983dd1599dd10d4f28
Nathan Typanski <mail@nathantypanski.com>
2014-10-21 03:21:25 -0400

    no more zsh syntax highlighter

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -8,7 +8,6 @@ setopt PROMPT_SUBST
 # # export PROMPT="%1~%(λ#.#.$BLUE$)$NO_COLOR "
 source ~/.zsh/git-prompt.zsh
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main pattern)
-source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 echo -ne "\033]12;6\007"
 PROMPT='%~/ %{$reset_color%}%{$terminfo[bold]$fg[blue]%}λ %{$reset_color%}'
 if [[ "$(whoami)" == "root" ]]; then
