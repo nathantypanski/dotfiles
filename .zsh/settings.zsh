65033fc12999c58d118e76a4c87eae2a4c6ad59d
Nathan Typanski <ntypanski@gmail.com>
2014-11-14 08:22:30 -0500

    don't start keychain in zsh

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -78,8 +78,6 @@ bindkey '^J' open-urxvt-here
 setopt vi
 setopt transient_rprompt
 
-#eval $(keychain --eval --agents ssh -Q --quiet $(cat ~/.keychain/keyfiles))
-
 # for sd_cmd in systemctl systemd-analyze systemd-run; do
 #     alias $sd_cmd='DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/dbus/user_bus_socket" '$sd_cmd
 # done
