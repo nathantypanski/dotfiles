0ee3a951efc00fa1bd3bb04bdb290dbaea4704cd
Nathan Typanski <ntypanski@gmail.com>
2014-11-15 15:27:05 -0500

    submodule repo strcture

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ stow -v --no-folding --target="$XDG_CONFIG_HOME/ranger" ranger
 stow -v --no-folding --target="$XDG_CONFIG_HOME" i3
 stow -v --no-folding --target="$XDG_CONFIG_HOME" systemd
 stow -v --no-folding --target="$XDG_CONFIG_HOME" herbstluftwm
+stow -v --no-folding --target="$HOME/bin" bin
 stow -v --no-folding --target="$HOME" mutt
 stow -v --no-folding --target="$HOME" zsh
 stow -v --no-folding --target="$HOME" xresources
 stow -v --no-folding --target="$HOME" tmux
 stow -v --no-folding --target="$HOME" vim
-stow -v --no-folding --target="$HOME/.weechat" weechat
+stow -v --target="$HOME" emacs
+stow -v --target="$HOME" xmonad
 stow -v --no-folding --target="$HOME" xbindkeys
-stow -v --no-folding --target="$HOME/bin" bin
+stow -v --no-folding --target="$HOME/.weechat" weechat
 
