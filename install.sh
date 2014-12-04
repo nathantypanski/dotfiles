22c60173507f9fb1f785d33c35c65e5f027f9dca
Nathan Typanski <ntypanski@gmail.com>
2014-12-03 23:28:54 -0500

    move systemd/systemd/user to systemd/user

--- install.sh
+++ install.sh
@@ -4,18 +4,20 @@ if [ -n "$XDG_CONFIG_HOME" ]; then
     XDG_CONFIG_HOME="$HOME"'/.config'
 fi
 
-stow -v --target="$XDG_CONFIG_HOME" systemd
-stow -v --no-folding --target="$XDG_CONFIG_HOME/ranger" ranger
-stow -v --no-folding --target="$XDG_CONFIG_HOME" i3
-stow -v --no-folding --target="$XDG_CONFIG_HOME" herbstluftwm
-stow -v --no-folding --target="$HOME/bin" bin
-stow -v --no-folding --target="$HOME" mutt
-stow -v --no-folding --target="$HOME" zsh
-stow -v --no-folding --target="$HOME" xresources
-stow -v --no-folding --target="$HOME" tmux
-stow -v --no-folding --target="$HOME" vim
-stow -v --target="$HOME" emacs
-stow -v --target="$HOME" xmonad
-stow -v --no-folding --target="$HOME" xbindkeys
-stow -v --no-folding --target="$HOME/.weechat" weechat
+IGNORE='^.dotfiles/?.*'
+
+stow -v --ignore="$IGNORE" --target="$XDG_CONFIG_HOME"'/systemd' 'systemd'
+stow -v --ignore="$IGNORE" --no-folding --target="$XDG_CONFIG_HOME"'/ranger' 'ranger'
+stow -v --ignore="$IGNORE" --no-folding --target="$XDG_CONFIG_HOME" 'i3'
+stow -v --ignore="$IGNORE" --no-folding --target="$XDG_CONFIG_HOME" 'herbstluftwm'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME"'/bin' 'bin'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'mutt'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'zsh'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'xresources'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'tmux'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'vim'
+stow -v --ignore="$IGNORE" --target="$HOME" 'emacs'
+stow -v --ignore="$IGNORE" --target="$HOME" 'xmonad'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'xbindkeys'
+stow -v --ignore="$IGNORE" --no-folding --target="$HOME/.weechat" 'weechat'
 
