224f7f7ecbf1dc71e343b15d60f2b843b6a6d3f3
Nathan Typanski <ntypanski@gmail.com>
2015-01-10 12:36:17 -0500

    stow: tweak ignores/env variables

--- install.sh
+++ install.sh
@@ -4,23 +4,30 @@ if [ -n "$XDG_CONFIG_HOME" ]; then
     XDG_CONFIG_HOME="$HOME"'/.config'
 fi
 
-IGNORE='^.dotfiles/?.*'
+# These commands are dangerous if this is unbound.
+[[ -z "$HOME" ]] && exit 1
 
-stow -v --ignore="$IGNORE" --target="$XDG_CONFIG_HOME"'/systemd' 'systemd'
-stow -v --ignore="$IGNORE" --no-folding --target="$XDG_CONFIG_HOME"'/ranger' 'ranger'
-stow -v --ignore="$IGNORE" --no-folding --target="$XDG_CONFIG_HOME" 'i3'
-stow -v --ignore="$IGNORE" --no-folding --target="$XDG_CONFIG_HOME" 'herbstluftwm'
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME"'/bin' 'bin'
+I1='^.dotfiles/?.*'
+I2='.*README.md'
+I3='.*LICENSE$'
 
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'mutt'
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'zsh'
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'xresources'
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'tmux'
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'vim'
-stow -v --ignore="$IGNORE" --target="$HOME" 'emacs'
-stow -v --ignore="$IGNORE" --target="$HOME" 'xmonad'
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME" 'xbindkeys'
-stow -v --ignore="$IGNORE" --no-folding --target="$HOME/.weechat" 'weechat'
+stow -v --ignore="$I1" --target="$XDG_CONFIG_HOME"'/systemd' 'systemd'
+stow -v --ignore="$I1" --no-folding --target="$XDG_CONFIG_HOME"'/ranger' 'ranger'
+stow -v --ignore="$I1" --no-folding --target="$XDG_CONFIG_HOME" 'i3'
+stow -v --ignore="$I1" --no-folding --target="$XDG_CONFIG_HOME" 'herbstluftwm'
+stow -v --ignore="$I1" --no-folding --target="$HOME"'/bin' 'bin'
+
+stow -v --ignore="$I1" --no-folding --target="$HOME" 'mutt'
+stow -v --ignore="$I1" --no-folding --target="$HOME" 'zsh'
+stow -v --ignore="$I1" --no-folding --target="$HOME" 'xresources'
+stow -v --ignore="$I1" --no-folding --target="$HOME" 'tmux'
+stow -v --ignore="$I1" --no-folding --target="$HOME" 'vim'
+stow -v --ignore="$I1" --target="$HOME" 'emacs'
+stow -v --ignore="$I1" --target="$HOME" 'xmonad'
+stow -v --ignore="$I1" --no-folding --target="$HOME" 'xbindkeys'
+stow -v --ignore="$I1" --no-folding --target="$HOME/.weechat" 'weechat'
+
+
 
 # individual script links
 ln -s $(pwd)'/displaylink.sh/displaylink.sh' "$HOME"'/bin' 
