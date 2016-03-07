9a8ea912938eabdda53e55a3838a450311b0d40a
Nathan Typanski <ntypanski@palantir.com>
2016-03-07 15:46:25 -0500

    add blurry lockscreen

--- bin/lockscreen.sh
+++ bin/lockscreen.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-import -window root png:- | convert - -blur 0x20 png:- | i3lock -n -f -i /dev/stdin
+# One-liner version:
+# import -window root png:- | convert - -blur 0x20 png:- | i3lock -n -f -i /dev/stdin
+scrot /tmp/lock.png
+i3lock -f -i /tmp/lock.png
+convert /tmp/lock.png -blur 0x8 png:- > /tmp/lockblur.png 
+killall i3lock
+i3lock -n -f -i /tmp/lockblur.png
