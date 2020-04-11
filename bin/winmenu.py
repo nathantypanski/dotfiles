4c46514020afabfbf4429592cdbd228e42b7ff3d
Nathan Typanski <mail@nathantypanski.com>
2020-04-11 15:44:10 -0400

    update emacs

--- bin/winmenu.py
+++ bin/winmenu.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
+# Source: https://github.com/xmendez/i3-scripts/blob/master/winmenu.py
+# License: not provided :(
+#
 # dmenu script to jump to windows in i3.
-# Makes a list of nicely formatted strings for each open window, containing
-# workspace name, mark (if any), window title, and instance (would the previous
-# give a non-unique result). dmenu provides a fast method of selection. 
-# If the result of dmenu is that there is no match for an existing window, 
-# the currently focused window will get a mark with this word. 
 #
 # using ziberna's i3-py library: https://github.com/ziberna/i3-py
 # depends: dmenu (vertical patch), i3.
@@ -12,25 +10,23 @@
 # http://sam.zoy.org/wtfpl/COPYING
 #
 # edited by Jure Ziberna for i3-py's examples section
-# and then again by Nathan Typanski for his personal dotfiles
+#
+# Trying to mimic ruby-wmii script behauvior
+# I used this on my config as follows:
+#
+# bindsym $mod+Shift+K exec python ~/.i3/scripts/winmenu.py
 
 import i3
 import subprocess
 
 def i3clients():
     """
-    Returns a dictionary with convoluted strings with window information as keys, 
-    and the i3 window id as values. Each window text is of format 
-    "[workspace] mark window title (instance number)."
+    Returns a dictionary of key-value pairs of a window text and window id.
+    Each window text is of format "[workspace] window title (instance number)"
     """
     clients = {}
-    lengths = {'workspace': 0, 'mark': 0}
-    tree = i3.get_tree()
-    for ws in i3.get_workspaces():
-        wsname = ws['name']
-        if len(wsname) > lengths['workspace']:
-            lengths['workspace'] = len(wsname)
-        workspace = i3.filter(tree, name=wsname)
+    for ws_num in range(1,11):
+        workspace = i3.filter(num=ws_num)
         if not workspace:
             continue
         workspace = workspace[0]
@@ -38,66 +34,37 @@ def i3clients():
         instances = {}
         # Adds windows and their ids to the clients dictionary
         for window in windows:
-            windowdict = {
-                    'con_id': window['id'], \
-                    'ws': wsname, \
-                    'name': window['name']}
-            try: 
-                windowdict['mark'] = window['mark']
-                if len(window['mark']) > lengths['mark']:
-                    lengths['mark'] = len(window['mark'])
-            except KeyError:
-                windowdict['mark'] = ""
-            if window['name'] in instances: 
-                instances[window['name']] += 1
-            else: 
-                instances[window['name']]  = 1
-            windowdict['instance'] = instances[window['name']]
-            # win_str = '[%s] %s' % (workspace['name'], window['name'])
-            clients[window['id']] = windowdict
-
-    # Now build the strings to pass to dmenu: 
-    newdict = {}
-    clientlist = []
-    for con_id in clients.keys():
-        clientlist.append(con_id)
-    for con_id in clientlist:
-        wslen = lengths['workspace']
-        mlen = lengths['mark']
-        win_str = '[{k:<{v}}] {l:<{w}} {m} ({n})'.format(\
-                k=clients[con_id]['ws'], v=wslen, \
-                l=clients[con_id]['mark'], w=mlen, \
-                m=clients[con_id]['name'], \
-                n=clients[con_id]['instance'])
-        clients[win_str] = clients[con_id]
-        del clients[con_id]
+            win_str = '[%s] %s' % (workspace['name'], window['name'])
+            # Appends an instance number if other instances are present
+            if win_str in instances:
+                instances[win_str] += 1
+                win_str = '%s (%d)' % (win_str, instances[win_str])
+            else:
+                instances[win_str] = 1
+            clients[win_str] = window['id']
     return clients
 
-
-def win_menu(clients, l=20):
+def win_menu(clients, l=10):
     """
-    Displays a window menu using dmenu.
+    Displays a window menu using dmenu. Returns window id.
     """
-    dmenu = subprocess.Popen(['dmenu',
-        '-fn', '-*-terminus-medium-*-*-*-16-*-*-*-*-*-*-*',
-        '-nb', '#1D1F21', '-sb', '#282A2E', '-nf', '#C5C8C6', '-sf', '#81A2BE',
-        '-p', '$', '-i', '-w', '800', '-x', '800', '-dim', '0.5',
-        '-l', str(l)],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE)
+    dmenu = subprocess.Popen(['dmenu_run.sh'])
+        # '-fn', '-*-terminus-medium-*-*-*-16-*-*-*-*-*-*-*',
+        # '-nb', '#1D1F21', '-sb', '#282A2E', '-nf', '#C5C8C6', '-sf', '#81A2BE',
+        # '-p', '$', '-i', '-w', '800', '-x', '800', '-dim', '0.5',
+        # '-l', str(l)],
+    #dmenu = subprocess.Popen(['/usr/bin/dmenu','-i','-l', str(l)],
+    # dmenu = subprocess.Popen(['/usr/bin/dmenu','-i','-b'],
+    # stdin=subprocess.PIPE,
+            # stdout=subprocess.PIPE)
     menu_str = '\n'.join(sorted(clients.keys()))
     # Popen.communicate returns a tuple stdout, stderr
-    win_str = dmenu.communicate(menu_str.encode('utf-8'))[0].decode().rstrip()
-    print(win_str)
-    return win_str
+    win_str = dmenu.communicate(menu_str.encode('utf-8'))[0].decode('utf-8').rstrip()
+    return clients.get(win_str, None)
 
 if __name__ == '__main__':
     clients = i3clients()
-    win_str = win_menu(clients)
-    try:
-        clients[win_str]
-    except:
-        #i3.mark(win_str)
-        raise SystemExit
-    con_id = clients[win_str]['con_id']
-    i3.focus(con_id=con_id)
+    win_id = win_menu(clients)
+    if win_id:
+        i3.focus(con_id=win_id)
+
