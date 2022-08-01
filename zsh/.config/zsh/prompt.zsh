127339996da8540f4401330e45daaa967134a2d3
ndt <ndt@nathantypanski.com>
2022-08-01 12:36:49 -0400

    zsh: rework prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -1,5 +1,6 @@
 # fancy zsh colors in prompt
 autoload -U colors && colors
 
-PROMPT="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}%# "
-RPROMPT="[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]"
+#PROMPT="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}$ "
+PROMPT="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}$ "
+RPROMPT="%{$reset_color%}[%(?.%{$fg_bold[green]%}.%{$fg_bold[red]%})%?%{$reset_color%}]"
