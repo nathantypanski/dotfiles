7a8efb92b3157b44042a9669b139ac4d96be1f6a
Nathan Typanski <mail@nathantypanski.com>
2025-04-24 10:25:51 -0400

    add darwin config:

---
+++ nix/.config/darwin/README.md
@@ -0,0 +1,12 @@
+# darwin flake
+
+To run:
+
+```bash
+$ nix run nix-darwin -- switch --flake ~/.config/nix
+```
+
+## References
+
+- [Home Manager Option Search](https://home-manager-options.extranix.com/)
+- 
---
+++ nix/.config/home-manager/darwin.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, username, homeDirectory, secrets, ... }:
+
+{
+  imports = [
+    ./zsh.nix
+    ./tmux.nix
+  ];
+
+  home.username = username;
+  home.homeDirectory = homeDirectory;
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.tmux
+  ];
+
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/davish/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  # programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = username;
+    userEmail = secrets.userEmail;
+  };
+}
