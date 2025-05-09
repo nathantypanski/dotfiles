ee2b5d69ca4c884719cda280bfa59b218e0d596f
ndt <mail@nathantypanski.com>
2025-05-08 22:20:48 -0400

    use latest git for lazy-trees (less copying flakes)

--- nix/.config/arch/rebuild.sh
+++ nix/.config/arch/rebuild.sh
@@ -6,4 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export NIXPKGS_ALLOW_UNFREE=1
 
-nix run github:nix-community/home-manager -- switch --impure --flake "${SCRIPT_DIR}"
+# use latest nix for lazy-trees patches
+nix shell nixpkgs#nixVersions.latest --command nix run \
+    github:nix-community/home-manager \
+    -- switch --impure --flake "${SCRIPT_DIR}"
