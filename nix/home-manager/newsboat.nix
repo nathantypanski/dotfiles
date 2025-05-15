32e93b7f46692e35fb88a0be9fcdfe38cc432277
ndt <mail@nathantypanski.com>
2025-05-15 15:13:45 -0400

    update rss tracking

--- nix/home-manager/newsboat.nix
+++ nix/home-manager/newsboat.nix
@@ -5,12 +5,14 @@
     enable = true;
     maxItems = 50;
     urls = [
-      { url = "https://nathantypanski.com/atom.xml"; }
+      { url = "https://words.filippo.io/rss/"; }
+      { url = "https://linderud.dev/blog/index.xml"; }
       { url = "https://krebsonsecurity.com/feed/atom"; }
       { url = "http://export.arxiv.org/api/query?search_query=cat:cs.CR&sortBy=submittedDate&sortOrder=descending&max_results=50"; }
       { url = "http://seclists.org/rss/fulldisclosure.rss"; }
       { url = "http://feeds.feedburner.com/Unit42"; }
       { url = "https://nedwill.github.io/blog/feed.xml"; }
+      { url = "https://nathantypanski.com/atom.xml"; }
     ];
   };
 }
