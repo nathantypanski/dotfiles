24eb047792ac51ecade30daf8da819e47f102d2a
ndt <mail@nathantypanski.com>
2025-06-04 19:35:58 -0400

    install mcp

--- nix/home-manager/arch.nix
+++ nix/home-manager/arch.nix
@@ -113,6 +113,9 @@ in {
     python313Packages.python-lsp-ruff
     poetry
 
+    #mcp
+    python313Packages.mcp
+
     (pkgs.writeShellScriptBin "pick-foot" ''
       exec ${pkgs.foot}/bin/foot --app-id=launcher --title=launcher -e 'bash' '-c' 'compgen -c | grep -v fzf | sort -u | fzf --layout=reverse | xargs -r swaymsg -t command exec'
     '')
