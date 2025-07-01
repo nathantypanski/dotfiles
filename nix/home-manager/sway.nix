0ae4551175560d3d903975a0cf7a04ff8c8945c3
ndt <mail@nathantypanski.com>
2025-06-30 23:44:03 -0400

    sway: slower trackpad accel

--- nix/home-manager/sway.nix
+++ nix/home-manager/sway.nix
@@ -227,7 +227,7 @@ in
           "2362:628:PIXA3854:00_093A:0274_Touchpad" = {
             scroll_factor = "0.5";
             accel_profile = "adaptive";
-            pointer_accel = "0.2";
+            pointer_accel = "0.1";
             dwt = "disabled";
             click_method = "clickfinger";
           };
