4cf6dfd2c344a54fa4cc6e88eb8bf32a5eb0dd73
ndt <mail@nathantypanski.com>
2025-08-01 03:53:32 -0400

    update rebuild script

--- arch/usr/local/bin/rebuild-ukis-for-sbctl.sh
+++ arch/usr/local/bin/rebuild-ukis-for-sbctl.sh
@@ -6,15 +6,18 @@ SWAP_UUID="$(blkid -s UUID -o value /dev/mapper/vg-swap)"
 UKI_PATH_ROOT="/boot/efi/EFI/Manual"
 DEFAULT_UKI_PATH="${UKI_PATH_ROOT}/arch.efi"
 TROUBLESHOOT_UKI_PATH="${UKI_PATH_ROOT}/arch-troubleshoot.efi"
+LTS_UKI_PATH="${UKI_PATH_ROOT}/arch-lts.efi"
+TROUBLESHOOT_LTS_UKI_PATH="${UKI_PATH_ROOT}/arch-lts-troubleshoot.efi"
 HARDENED_UKI_PATH="${UKI_PATH_ROOT}/arch-hardened.efi"
 TROUBLESHOOT_HARDENED_UKI_PATH="${UKI_PATH_ROOT}/arch-hardened-troubleshoot.efi"
 
 # usbcore quirks - fix Goodix Fingerprint USB Device
-COMMON_CMDLINE="root=/dev/mapper/vg-main rw rootflags=subvol=@ usbcore.quirks=27c6:609c:0x40 rd.luks.name=${LUKS_UUID}=cryptroot rd.luks.options=${LUKS_UUID}=tpm2-device=auto,tpm2-measure-pcr=yes,discard,tries=3 lsm=capability,landlock,lockdown,yama,apparmor,bpf mitigations=full"
+# nvme_core.default_ps_max_latency_us=
+COMMON_CMDLINE="root=/dev/mapper/vg-main rw rootflags=subvol=@ usbcore.quirks=27c6:609c:0x40 rd.luks.name=${LUKS_UUID}=cryptroot rd.luks.options=${LUKS_UUID}=tpm2-device=auto,tpm2-measure-pcr=yes,discard,tries=3 lsm=capability,landlock,lockdown,yama,apparmor,bpf mitigations=full nvme_core.default_ps_max_latency_us=25000"
 SECURE_CMDLINE="audit=1 iommu=pt"
 SUSPEND_CMDLINE="rtc_cmos.use_acpi_alarm=1"
 RESUME_CMDLINE="resume=UUID=${SWAP_UUID}"
-TROUBLE_CMDLINE="log_level=7 rd.debug"
+TROUBLE_CMDLINE="log_level=7 rd.debug no_console_suspend"
 
 SIGN_TOOL=sbsign
 SECUREBOOT_PRIVATE_KEY="/var/lib/sbctl/keys/db/db.key"
@@ -29,18 +32,14 @@ echo "  ${COMMON_CMDLINE}"
 echo "hardened cmdline:"
 echo "  ${COMMON_CMDLINE} ${SECURE_CMDLINE}"
 
-MEASUREMENTS_DIR=/etc/systemd/measurements
-mkdir -p "${MEASUREMENTS_DIR}"
-
 build_uki() {
     local kernel_path="$1"
     local initrd_path="$2"
     local cmdline="$3"
     local output_path="$4"
-    local predictions_file="$5"
-    local pcr15_value="${6:-}"
+    local pcr15_value="${5:-}"
 
-
+    set -x
     # Add expected PCR 15 to kernel command line
     if [[ -n "${pcr15_value}" ]]; then
         echo "got pcr15 value: ${pcr15_value}"
@@ -50,6 +49,7 @@ build_uki() {
     local cmdline_file="$(mktemp)"
     echo "$cmdline" > "${cmdline_file}"
     trap "rm -f '${cmdline_file}'; echo >&2 'Cleaned up temp file: ${cmdline_file}'" EXIT
+        # --pcrs=0,6,7 \
 
     echo "Building $(basename "$output_path")..."
     ukify build \
@@ -64,37 +64,49 @@ build_uki() {
         --secureboot-private-key="${SECUREBOOT_PRIVATE_KEY}" \
         --secureboot-certificate="${SECUREBOOT_CERTIFICATE}" \
         --output="${output_path}"
-
-    # sbctl sign -s "$output_path"
+    set +x
 }
 
-echo "Rebuilding ${DEFAULT_UKI_PATH}..."
 build_uki \
     "/boot/vmlinuz-linux" \
     "/boot/initramfs-linux.img" \
-    "${COMMON_CMDLINE} ${SUSPEND_CMDLINE} ${RESUME_CMDLINE}" \
+    "${COMMON_CMDLINE} ${SUSPEND_CMDLINE} ${RESUME_CMDLINE} no_console_suspend rd.debug" \
     "${DEFAULT_UKI_PATH}" \
-    "/etc/systemd/predicted-pcrs-arch.json"
+    '98f31cd3ddfde2e5c8a1f6ce90a078bd0b6a3a66ca39133ec5b6a26b7e10925a'
 
 build_uki \
     "/boot/vmlinuz-linux" \
     "/boot/initramfs-linux-fallback.img" \
     "${COMMON_CMDLINE} ${SUSPEND_CMDLINE} ${TROUBLE_CMDLINE}" \
     "${TROUBLESHOOT_UKI_PATH}" \
-    "/etc/systemd/predicted-pcrs-arch-troubleshoot.json"
+    '98f31cd3ddfde2e5c8a1f6ce90a078bd0b6a3a66ca39133ec5b6a26b7e10925a'
+
+build_uki \
+    "/boot/vmlinuz-linux-lts" \
+    "/boot/initramfs-linux-lts.img" \
+    "${COMMON_CMDLINE} ${SUSPEND_CMDLINE} ${RESUME_CMDLINE} ${TROUBLE_CMDLINE} no_console_suspend rd.debug" \
+    "${LTS_UKI_PATH}" \
+    '98f31cd3ddfde2e5c8a1f6ce90a078bd0b6a3a66ca39133ec5b6a26b7e10925a'
+
+build_uki \
+    "/boot/vmlinuz-linux-lts" \
+    "/boot/initramfs-linux-lts-fallback.img" \
+    "${COMMON_CMDLINE} ${SUSPEND_CMDLINE} ${TROUBLE_CMDLINE}" \
+    "${TROUBLESHOOT_LTS_UKI_PATH}" \
+    '98f31cd3ddfde2e5c8a1f6ce90a078bd0b6a3a66ca39133ec5b6a26b7e10925a'
 
 build_uki \
     "/boot/vmlinuz-linux-hardened" \
     "/boot/initramfs-linux-hardened.img" \
     "${COMMON_CMDLINE} ${SUSPEND_CMDLINE} ${SECURE_CMDLINE}" \
     "${HARDENED_UKI_PATH}" \
-    "/etc/systemd/predicted-pcrs-arch-hardened.json"
+    '98f31cd3ddfde2e5c8a1f6ce90a078bd0b6a3a66ca39133ec5b6a26b7e10925a'
 
 build_uki \
     "/boot/vmlinuz-linux-hardened" \
     "/boot/initramfs-linux-hardened-fallback.img" \
     "${COMMON_CMDLINE} ${SUSPEND_CMDLINE} ${TROUBLE_CMDLINE}" \
     "${TROUBLESHOOT_HARDENED_UKI_PATH}" \
-    "/etc/systemd/predicted-pcrs-arch-hardened-troubleshoot.json"
+    '98f31cd3ddfde2e5c8a1f6ce90a078bd0b6a3a66ca39133ec5b6a26b7e10925a'
 
 echo "UKIs rebuilt and signed."
