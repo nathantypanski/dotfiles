ee0a5149784bf35376d48e58d168aef6f4e21ff5
ndt <mail@nathantypanski.com>
2025-09-05 22:21:50 -0400

    add experimental fast builds

--- nix/arch/rebuild.sh
+++ nix/arch/rebuild.sh
@@ -19,8 +19,10 @@ mkdir -p "${MANUAL_GCROOTS_DIR}"
 
 (
   echo "$SCRIPT_DIR"
-  cd "${SCRIPT_DIR}" && nix shell nixpkgs#nixVersions.latest \
-    --command nix build --impure "${SCRIPT_DIR}#home-manager-activation"
+  # cd "${SCRIPT_DIR}" && nix shell nixpkgs#nixVersions.latest \
+  cd "${SCRIPT_DIR}" && nix build "${@}" --impure "${SCRIPT_DIR}#home-manager-activation"
+  # Need to sort out warning: ignoring the client-specified setting 'max-substitution-jobs', because it is a restricted setting and you are not a trusted user
+  # cd "${SCRIPT_DIR}" && nix build --offline --impure --substitute --max-substitution-jobs 0 "${SCRIPT_DIR}#home-manager-activation"
 )
 
 ln -sf "$(readlink "${SCRIPT_DIR}/result")" "${MANUAL_GCROOTS_DIR}"
