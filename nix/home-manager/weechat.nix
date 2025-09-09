8531a0ccbc67fe76352e3db335927719677fd205
ndt <mail@nathantypanski.com>
2025-09-09 04:31:26 -0400

    install weechat via home packages + plugins

--- nix/home-manager/weechat.nix
+++ nix/home-manager/weechat.nix
@@ -1,26 +1,28 @@
 { config, pkgs, lib, ... }:
 
 {
-  programs.weechat = {
-    enable = true;
-    
-    # Install WeeChat with the Slack plugin
-    scripts = with pkgs.weechatScripts; [
-      wee-slack  # Slack plugin for WeeChat
-    ];
-    
-    # Optional: Additional useful scripts
-    # scripts = with pkgs.weechatScripts; [
-    #   wee-slack
-    #   multiline      # Multi-line editing
-    #   colorize-nicks # Colorize nicknames
-    #   go             # Quick buffer switching
-    #   buffer-autoset # Auto-set buffer properties
-    # ];
-  };
+  # Install WeeChat with plugins via home.packages
+  home.packages = [
+    (pkgs.weechat.override {
+      configure = { availablePlugins, ... }: {
+        scripts = with pkgs.weechatScripts; [
+          wee-slack      # Slack plugin for WeeChat
+          # multiline    # Multi-line editing
+          # colorize_nicks # Colorize nicknames  
+          # go           # Quick buffer switching
+        ];
+        plugins = with availablePlugins; [
+          python
+          perl
+          ruby
+          lua
+          guile
+        ];
+      };
+    })
+  ];
 
   # Optional: Custom WeeChat configuration
-  # You can add config files here if needed
   # home.file.".weechat/weechat.conf".text = ''
   #   [general]
   #   buffer_auto_renumber = on
@@ -32,7 +34,4 @@
   #   [network]
   #   gnutls_priorities = "NORMAL:-VERS-SSL3.0"
   # '';
-
-  # Optional: Slack plugin configuration
-  # home.file.".weechat/python/autoload/wee_slack.py".source = "${pkgs.weechatScripts.wee-slack}/share/wee-slack.py";
 }
